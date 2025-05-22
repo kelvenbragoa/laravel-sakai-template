86bf14163f0026baf62ef7e2adfc4ff86bef2784
34sea <domingosmartinho67@gmail.com>
2025-05-22 09:54:19 +0200

    update list transacao cgate2

--- resources/js/api/index.js
+++ resources/js/api/index.js
@@ -50,20 +50,21 @@ export const getCarga = async (page, limit, search, status) => {
       throw error; 
     }
   };
-// const baseURLCDMProd = "https://cdmapi.cornelder.co.mz/cgate1x/api"
-const baseURLCDMProd = "http://10.0.8.44:8010/api"
+const baseURLCDMProd = "https://cdmapi.cornelder.co.mz/cgate1x/api"
+// const baseURLCDMProd = "http://10.0.8.44:8010/api"
 // const baseURLCDMProd = "http://127.0.0.1:8000/api"
 
 const storageImg =  "https://cdmapi.cornelder.co.mz/cgate1x"
 //cgate2 - terminal
-const baseURLCgate2 = "http://20.87.9.35/api/v1/transacoes"
+// const baseURLCgate2 = "http://20.87.9.35/api/v1/transacoes"
+const baseURLCgate2 = "https://cdmapi.cornelder.co.mz/cgate1x"
 //cgate2 - carga
 const baseURLCgate2Carga = "https://cdmapi.cornelder.co.mz"
 
   export const baseUrls = {
     auth: `${baseURLCDMProd}/login`,
     transacoes: `${baseURLCDMProd}/containertransaction`,
-    transacoesCgate2dotzero: `${baseURLCgate2}/lista`,
+    transacoesCgate2dotzero: `${baseURLCgate2}/cgate2/transaction`,
     transacoesCgate2dotzeroCarga: `${baseURLCgate2Carga}/cgate/api/v1/c_gate/general_cargo/list_transactions`,
     userList: `${baseURLCDMProd}/users`,
     empresaAdd: `${baseURLCDMProd}/companies`,
